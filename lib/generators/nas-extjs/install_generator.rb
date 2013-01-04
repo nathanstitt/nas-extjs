a1340055c7bcf8b0d188c86ec5bcec33ccfa9380
Nathan Stitt <nathan@stitt.org>
2013-01-03 21:44:44 -0600

    generator

--- lib/generators/nas-extjs/install_generator.rb
+++ lib/generators/nas-extjs/install_generator.rb
@@ -2,21 +2,25 @@ require 'rails/generators'
 
 module NasExtjs
 
-    class InstallGenerator < Rails::Generators::Base
-        desc "Some description of my generator here"
+    module Generators
 
-        # Commandline options can be defined here using Thor-like options:
-        class_option :my_opt, :type => :boolean, :default => false, :desc => "My Option"
+        class InstallGenerator < Rails::Generators::Base
+            desc "Some description of my generator here"
 
-        # I can later access that option using:
-        # options[:my_opt]
+            # Commandline options can be defined here using Thor-like options:
+            class_option :my_opt, :type => :boolean, :default => false, :desc => "My Option"
+
+            # I can later access that option using:
+            # options[:my_opt]
 
 
-        def self.source_root
-            @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+            def self.source_root
+                @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+            end
+
+            # Generator Code. Remember this is just suped-up Thor so methods are executed in order
         end
 
-        # Generator Code. Remember this is just suped-up Thor so methods are executed in order
     end
 
 end
