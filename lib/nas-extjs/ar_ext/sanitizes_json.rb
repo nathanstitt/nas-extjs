4a7b112063521a1290e1b15f01e6b38ac4c8accb
Nathan Stitt <nathan@stitt.org>
2013-01-18 17:13:57 -0600

    Rename hidden methods and straighten out ternary

--- lib/nas-extjs/ar_ext/sanitizes_json.rb
+++ lib/nas-extjs/ar_ext/sanitizes_json.rb
@@ -23,34 +23,38 @@ module NasExtjs::ArExt
 
 
                     if (
-                        ! _is_blacklisted?( kv.first ) && (
+                        ! _api_is_blacklisted?( kv.first ) && (
                                                            ( self.attribute_names && self.attribute_names.include?(key) ) ||
                                                            ( self.allowed_attributes && self.allowed_attributes.include?(key) ) ||
                                                            ( key == '_destroy')
                                                            ) )
                         ret[key] = val
-                    elsif _is_exported?( key ) && _accepts_nested?( key )
+                    elsif _api_is_exported?( key ) && _api_accepts_nested?( key )
 
                         klass = self.reflections[ key.sub('_attributes','').to_sym ].class_name.constantize
                         # only Hash, Array & nil is valid for nesting attributes
                         next if val.nil?
 
-                        ret[key] = val.is_a?( Hash ) ? klass.api_sanitize_json( val ) : val.map{ | data | klass.api_sanitize_json( data ) }
+                        ret[key] = if val.is_a?( Hash ) 
+                                       klass.api_sanitize_json( val )
+                                   else
+                                       val.map{ | data | klass.api_sanitize_json( data ) }
+                                   end
                     end
 
                 end
 
             end
 
-            def _is_blacklisted?(name)
+            def _api_is_blacklisted?(name)
                 blacklisted_json_attributes.include?( name )
             end
 
-            def _is_exported?( name )
+            def _api_is_exported?( name )
                 self.exported_associations && self.exported_associations.find{|n| n.to_s + '_attributes' == name }
             end
 
-            def _accepts_nested?(name)
+            def _api_accepts_nested?(name)
                 self.nested_attributes_options? && self.nested_attributes_options[ name.sub('_attributes','').to_sym ]
             end
 
