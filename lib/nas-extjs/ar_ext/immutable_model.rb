18c1b3e47cace312fdf426d75ba80709fdf0e3dd
Nathan Stitt <nathan@stitt.org>
2013-08-16 18:32:23 -0500

    whitespace

--- lib/nas-extjs/ar_ext/immutable_model.rb
+++ lib/nas-extjs/ar_ext/immutable_model.rb
@@ -10,13 +10,13 @@ module NasExtjs::ArExt
 
                 options[:except] = [*options[:except]].map{|name| name.to_s } # make sure except is present and an array
 
-                before_destroy do 
-                    raise ActiveRecord::ReadOnlyRecord.new( "Can not destroy #{self.class.model_name}, only create is allowed" ) 
+                before_destroy do
+                    raise ActiveRecord::ReadOnlyRecord.new( "Can not destroy #{self.class.model_name}, only create is allowed" )
                 end
 
                 before_update do
                     unless ( changes.keys - options[:except] ).blank?
-                        raise ActiveRecord::ReadOnlyRecord.new(  "Can not update, only create #{self.class.model_name}" ) 
+                        raise ActiveRecord::ReadOnlyRecord.new(  "Can not update, only create #{self.class.model_name}" )
                     end
                 end
 
